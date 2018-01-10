a0acbf39bd04c52fd2d69496bc29a505b2c9d0bb
David <david.dmr@gmail.com>
2018-01-09 23:19:31 -0300

    Creating parcelable content for Accessory class

--- app/src/main/java/br/com/david/inelviskotlinstudy/domain/Accessory.kt
+++ app/src/main/java/br/com/david/inelviskotlinstudy/domain/Accessory.kt
@@ -1,6 +1,29 @@
 package br.com.david.inelviskotlinstudy.domain
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Accessory(
         val name: String,
-        val price: Float
-)
+        val price: Float) : Parcelable {
+
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readFloat()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(name)
+        writeFloat(price)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Accessory> = object : Parcelable.Creator<Accessory> {
+            override fun createFromParcel(source: Parcel): Accessory = Accessory(source)
+            override fun newArray(size: Int): Array<Accessory?> = arrayOfNulls(size)
+        }
+    }
+}
