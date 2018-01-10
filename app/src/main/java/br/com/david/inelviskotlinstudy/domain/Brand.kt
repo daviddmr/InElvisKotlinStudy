f49273ba80141ef1890045b6ed3d95a06a5de79d
David <david.dmr@gmail.com>
2018-01-09 23:20:11 -0300

    Creating parcelable content for Brand class

--- app/src/main/java/br/com/david/inelviskotlinstudy/domain/Brand.kt
+++ app/src/main/java/br/com/david/inelviskotlinstudy/domain/Brand.kt
@@ -1,6 +1,29 @@
 package br.com.david.inelviskotlinstudy.domain
 
+import android.os.Parcel
+import android.os.Parcelable
+
 class Brand(
         val name: String,
-        val logo: Int
-)
+        val logo: Int) : Parcelable {
+
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readInt()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(name)
+        writeInt(logo)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Brand> = object : Parcelable.Creator<Brand> {
+            override fun createFromParcel(source: Parcel): Brand = Brand(source)
+            override fun newArray(size: Int): Array<Brand?> = arrayOfNulls(size)
+        }
+    }
+}
