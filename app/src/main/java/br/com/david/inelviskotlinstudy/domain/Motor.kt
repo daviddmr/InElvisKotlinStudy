398fe515f64ffae49c9f1a510293465201079605
David <david.dmr@gmail.com>
2018-01-09 23:21:18 -0300

    Creating parcelable content for Motor class

--- app/src/main/java/br/com/david/inelviskotlinstudy/domain/Motor.kt
+++ app/src/main/java/br/com/david/inelviskotlinstudy/domain/Motor.kt
@@ -1,7 +1,32 @@
 package br.com.david.inelviskotlinstudy.domain
 
+import android.os.Parcel
+import android.os.Parcelable
+
 class Motor(
         val model: String,
         val cylinders: Int,
-        val brand: String
-)
+        val brand: String) : Parcelable {
+    
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readInt(),
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(model)
+        writeInt(cylinders)
+        writeString(brand)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Motor> = object : Parcelable.Creator<Motor> {
+            override fun createFromParcel(source: Parcel): Motor = Motor(source)
+            override fun newArray(size: Int): Array<Motor?> = arrayOfNulls(size)
+        }
+    }
+}
